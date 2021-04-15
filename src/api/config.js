191691ad14c8f4a3dcb409ef38c63d132a3cedc3
Bryan Amaguaña <bryanamaguana@MacBook-Pro-de-Bryan.local>
2021-04-15 14:27:31 -0500

    Actualizacion 2

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-export const basePath = "http://localhost:3050";
-/* export const basePath = "https://servicios-union.herokuapp.com"; */
+//export const basePath = "http://localhost:3050";
+export const basePath = "https://servicios-union.herokuapp.com";
 export const apiVersion = "v1";
